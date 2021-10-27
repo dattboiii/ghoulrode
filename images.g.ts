3429fd3d5da2c16739831a5665154321a5721572
dattboiii <93279859+dattboiii@users.noreply.github.com>
2021-10-27 13:11:02 -0400

    Initial files for MakeCode project

---
+++ images.g.ts
@@ -0,0 +1,37 @@
+// Auto-generated code. Do not edit.
+namespace myImages {
+
+    helpers._registerFactory("image", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+            case "image1":
+            case "mega blsat":return img`
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . 8 8 8 8 8 8 8 . . . . 
+. . . . 8 9 6 c 6 9 8 8 8 . . . 
+. . . . 8 6 6 c 9 6 9 9 8 . . . 
+. . . . 8 9 9 c c 9 6 6 8 . . . 
+. . . . 8 6 9 9 c c 6 9 8 . . . 
+. . . . 8 6 6 9 9 c 9 6 8 . . . 
+. . . . 8 6 6 6 9 c 6 9 8 . . . 
+. . . . 8 6 6 6 9 9 c 9 8 . . . 
+. . . . . 8 8 8 8 8 8 8 . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+`;
+        }
+        return null;
+    })
+
+    helpers._registerFactory("animation", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+
+        }
+        return null;
+    })
+
+}
+// Auto-generated code. Do not edit.
